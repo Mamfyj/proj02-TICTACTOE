4c79fd37859ddfdba21f45d169e66e9590030100
Mauro Moreno <Mauro>
2025-02-17 18:10:15 -0300

    TIC-TAC-TOE Exercise, fully functional. Missing win screen.

--- index.js
+++ index.js
@@ -1,22 +1,68 @@
-const menu = [
-    {name: "Margherita", price: 100},
-    {name: "Calabresa", price: 100},
-    {name: "Mozzarella", price: 100},
-    {name: "Portuguese", price: 100},
-    {name: "Chicken n' Catupiry", price: 100}
-]
-
-const cashInReg = 1000
-const orderQueue = []
-
-function addPizza(pizza){
-    menu.push(pizza)
+"use strict";
+const board = document.querySelector("#board");
+let round = 1;
+let gameOver = false;
+function createBoard() {
+    round = 1;
+    let i = 0;
+    for (i; i < 9; i++) {
+        console.log(i);
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.setAttribute('square-id', String(i));
+        square.addEventListener("click", onClick);
+        //i%2==0?square.innerHTML = iconO:square.innerHTML = iconX
+        board === null || board === void 0 ? void 0 : board.append(square);
+    }
 }
-
-function placeOrder(order){
-    let orderPizza = menu.find(pizza => pizza.name === order)
-    cashInReg += orderPizza.price
-    let newOrder = {pizza: orderPizza, startus: "Ordered"}
-    orderQueue.push(newOrder)
-    return newOrder
+createBoard();
+const allSquares = document.querySelectorAll('.square');
+function onClick(e) {
+    if (gameOver)
+        return;
+    const target = e.target;
+    if (target === e.currentTarget && target.getAttribute('square-id') && !target.innerHTML) {
+        console.log("I've been clicked." + target.getAttribute('square-id'));
+        target.innerHTML = round % 2 == 0 ? iconO : iconX;
+        round++;
+        getGameState();
+    }
+}
+function getGameState() {
+    //translate game state into an readable array
+    let temp = [];
+    allSquares.forEach(square => {
+        let div = square.querySelector('div');
+        if (div === null || div === void 0 ? void 0 : div.classList.contains("x")) {
+            temp.push(1);
+        }
+        else if (div === null || div === void 0 ? void 0 : div.classList.contains("o")) {
+            temp.push(2);
+        }
+        else {
+            temp.push(0);
+        }
+    });
+    //check array for wins.
+    for (let j = 0; j < 7; j += 3) {
+        if (temp[j] != 0 && temp[j] == temp[j + 1] && temp[j] == temp[j + 2]) {
+            gameWon();
+        }
+    }
+    for (let j = 0; j < 3; j++) {
+        if (temp[j] != 0 && temp[j] == temp[j + 3] && temp[j] == temp[j + 6]) {
+            gameWon();
+        }
+    }
+    if (temp[0] != 0 && temp[0] == temp[4] && temp[0] == temp[8]) {
+        gameWon();
+    }
+    if (temp[2] != 0 && temp[2] == temp[4] && temp[2] == temp[6]) {
+        gameWon();
+    }
+}
+function gameWon() {
+    gameOver = true;
+    let winner = round % 2 === 0 ? "X" : "O";
+    console.log(winner + " Wins!");
 }
---
+++ index.ts
@@ -0,0 +1,74 @@
+const board = document.querySelector("#board")
+let round: number = 1
+let gameOver: boolean = false
+
+function createBoard(){
+    round = 1
+    let i:number = 0
+    for(i; i<9; i++){
+        console.log(i)
+        const square = document.createElement('div')
+        square.classList.add('square')
+        square.setAttribute('square-id', String(i));
+        square.addEventListener("click", onClick)
+        //i%2==0?square.innerHTML = iconO:square.innerHTML = iconX
+        
+        board?.append(square)
+    }
+}
+
+createBoard()
+
+const allSquares = document.querySelectorAll('.square')
+
+function onClick(e: MouseEvent){
+    if (gameOver) return
+
+    const target = e.target as HTMLElement;
+    if (target === e.currentTarget && target.getAttribute('square-id') && !target.innerHTML){
+        console.log("I've been clicked." + target.getAttribute('square-id'))
+        target.innerHTML = round % 2 == 0 ? iconO : iconX;
+        round++
+        getGameState()
+    }
+}
+
+function getGameState(){
+    //translate game state into an readable array
+    let temp:number[] = []
+    allSquares.forEach(square =>{
+        let div = square.querySelector('div')
+        if (div?.classList.contains("x")) {
+            temp.push(1);
+        } else if (div?.classList.contains("o")) {
+            temp.push(2);
+        } else {
+            temp.push(0);
+        }
+    })
+
+    //check array for wins.
+    for(let j = 0; j<7; j+=3){
+        if(temp[j] != 0 && temp[j] == temp[j+1] && temp[j] == temp[j+2]){
+            gameWon()
+        }
+    }
+    for(let j = 0; j<3; j++){
+        if(temp[j] != 0 && temp[j] == temp[j+3] && temp[j] == temp[j+6]){
+            gameWon()
+        }
+    }
+    if (temp[0] != 0 && temp[0] == temp[4] && temp[0] == temp[8]) {
+        gameWon()
+    }
+    if (temp[2] != 0 && temp[2] == temp[4] && temp[2] == temp[6]) {
+        gameWon()
+    }
+}
+
+function gameWon(){
+    gameOver = true
+    let winner:string = round%2===0?"X" : "O"
+    console.log(winner + " Wins!")
+    
+}
